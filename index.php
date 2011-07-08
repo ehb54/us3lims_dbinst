b19595f7fb4e150309d445d5622374e338a8661e
zollarsd <zollarsd@bf3aa11d-60f9-42ff-b629-36e157aa8545>
2011-07-08 21:46:54 +0000

    Deleted some help files moved elsewhere

--- index.php
+++ index.php
@@ -86,21 +86,6 @@ submission website</a>.</p>
 Associate Professor<br/> 
 UltraScan Project Director</p>
 
-<h3>The UltraScan TeraGrid Science Gateway and LIMS system is supported by the
-following Institutions:</h3>
-<table cellspacing='0px' cellpadding='0px' class='imagelinks'>
-  <tr><td><img src='images/teragrid.png' alt='Teragrid logo' /></td>
-      <td><img src='images/TACC.gif' alt='TACC logo' /></td></tr>
-  <tr><td><a href='http://www.teragrid.org/'>Teragrid</a></td>
-      <td><a href='http://www.tacc.utexas.edu'>
-             Texas Advanced<br />Computing Center</a></td></tr>
-  <tr><td style='padding-top:2em;'><img src='images/uthscsa.gif' alt='UTHSCSA logo' /></td>
-      <td><img src='images/iu.png' alt='Indiana University logo' /></td></tr>
-  <tr><td><a href='http://www.uthscsa.edu'>
-              The University of Texas<br />Health Science Center</a></td>
-		<td><a href='http://www.iu.edu/'>Indiana University</a></td></tr>
-</table>
-
 </div>
 
 <?php
