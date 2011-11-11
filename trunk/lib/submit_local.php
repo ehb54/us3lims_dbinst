0c0ac426e7e5ccd45c94664d10ec68783e6070b5
zollarsd <zollarsd@bf3aa11d-60f9-42ff-b629-36e157aa8545>
2011-11-11 21:10:04 +0000

    Updated job submission to handle local better

--- trunk/lib/submit_local.php
+++ trunk/lib/submit_local.php
@@ -9,15 +9,8 @@ require_once 'lib/jobsubmit.php';
 
 class submit_local extends jobsubmit
 { 
-   private $workdir;
-   private $tarfile;
-   private $requestID;
-   private $port;
-   private $address;
-   private $cluster;
-   
-    // Submit the this->data
-    function submit()
+   // Submits data
+   function submit()
    {
       // a preliminary test to see if data is still defined
       if ( ! isset( $this->data[ 'job' ][ 'cluster_shortname' ] ) )
@@ -32,65 +25,63 @@ class submit_local extends jobsubmit
       $this->submit_job    ();
       $this->update_db     ();
       chdir( $savedir );
-$this->message[] = "End of submit_local.php\n";
+$this->message[] = "End of submit_local.php";
    }
  
    // Copy needed files to supercomputer
    function copy_files()
    {
-      $this->cluster   = $this->data[ 'job' ][ 'cluster_shortname' ];
-      $this->requestID = $this->data[ 'job' ][ 'requestID' ];
-      $jobid           = $this->data[ 'db' ][ 'name' ] . sprintf( "-%06d", $this->requestID );
-      $this->workdir   = $this->grid[ $this->cluster ][ 'workdir' ] . $jobid;
-      $this->address   = $this->grid[ $this->cluster ][ 'name' ];
-      $this->port      = $this->grid[ $this->cluster ][ 'sshport' ]; 
-      $this->tarfile   = sprintf( "hpcinput-%s-%s-%05d.tar",
-                               $this->data['db']['host'],
-                               $this->data['db']['name'],
-                               $this->data['job']['requestID'] );
-      
+      $cluster   = $this->data[ 'job' ][ 'cluster_shortname' ];
+      $requestID = $this->data[ 'job' ][ 'requestID' ];
+      $jobid     = $this->data[ 'db' ][ 'name' ] . sprintf( "-%06d", $requestID );
+      $workdir   = $this->grid[ $cluster ][ 'workdir' ] . $jobid;
+      $address   = $this->grid[ $cluster ][ 'name' ];
+      $port      = $this->grid[ $cluster ][ 'sshport' ]; 
+      $tarfile   = sprintf( "hpcinput-%s-%s-%05d.tar",
+                         $this->data['db']['host'],
+                         $this->data['db']['name'],
+                         $this->data['job']['requestID'] );
+    
       // Create working directory
       $output = array();
-      $cmd    = "ssh -p $this->port -x us3@$this->address mkdir -p $this->workdir 2>&1";
-
-$this->message[] = "cmd: $cmd\n";
-
+      $cmd    = "ssh -p $port -x us3@$address mkdir -p $workdir 2>&1";
       exec( $cmd, $output, $status );
 
-$this->message[] = "cmd output: " . implode( "\n", $output ) . "\n";
- 
       // Copy tar file
-      $cmd    = "scp -P $this->port $this->tarfile us3@$this->address:$this->workdir 2>&1";
-
-$this->message[] = "cmd: $cmd\n";
+      $cmd    = "scp -P $port $tarfile us3@$address:$workdir 2>&1";
       exec( $cmd, $output, $status );
-$this->message[] = "cmd output: " . implode( "\n", $output ) . "\n";
 
-      //  Create pbs file
+      //  Create and copy pbs file
       $pbsfile = $this->create_pbs();
-      $cmd     = "scp -P $this->port $pbsfile us3@$this->address:$this->workdir 2>&1";
-
-$this->message[] = "cmd: $cmd\n";
+      $cmd     = "scp -P $port $pbsfile us3@$address:$workdir 2>&1";
       exec( $cmd, $output, $status );
-$this->message[] = "cmd output: " . implode( "\n", $output ) . "\n";
       
-$this->message[] = "Files copied";
+$this->message[] = "Files copied to $address:$workdir";
    }
  
    // Create a pbs file
    function create_pbs()
    {
+      $cluster   = $this->data[ 'job' ][ 'cluster_shortname' ];
+      $requestID = $this->data[ 'job' ][ 'requestID' ];
+      $jobid     = $this->data[ 'db' ][ 'name' ] . sprintf( "-%06d", $requestID );
+      $workdir   = $this->grid[ $cluster ][ 'workdir' ] . $jobid;
+      $tarfile   = sprintf( "hpcinput-%s-%s-%05d.tar",
+                         $this->data['db']['host'],
+                         $this->data['db']['name'],
+                         $this->data['job']['requestID'] );
+
       $pbsfile = "us3.pbs";
       $wall    = $this->maxwall();
       $nodes   = $this->nodes();
 
       $hours   = (int)( $wall / 60 );
       $mins    = (int)( $wall % 60 );
-      $ppn     = $this->grid[ $this->cluster ][ 'ppn' ]; 
+      $ppn     = $this->grid[ $cluster ][ 'ppn' ]; 
 
       $walltime = sprintf( "%02.2d:%02.2d:00", $hours, $mins );  // 01:09:00
 
-      switch( $this->cluster )
+      switch( $cluster )
       {
         case 'bcf-local':
          $libpath = "/share/apps64/openmpi/lib";
@@ -117,8 +108,8 @@ $this->message[] = "Files copied";
       "#PBS -N US3_Job\n"                                   .
       "#PBS -l nodes=$nodes:ppn=$ppn,walltime=$walltime\n"           .
       "#PBS -V\n"                                           .
-      "#PBS -o $this->workdir/stdout\n"                     .
-      "#PBS -e $this->workdir/stderr\n"                     .
+      "#PBS -o $workdir/stdout\n"                     .
+      "#PBS -e $workdir/stderr\n"                     .
       "\n"                                                  .
       "export LD_LIBRARY_PATH=$libpath:\$LD_LIBRARY_PATH\n" .
       "export PATH=$path:\$PATH\n"                          .
@@ -128,8 +119,8 @@ $this->message[] = "Files copied";
       "export OMPI_MCA_mpi_show_handle_leaks=0\n"           .
       "export OMPI_MCA_mpi_show_mca_params=0\n"             .
       "\n"                                                  .
-      "cd $this->workdir\n"                                 .
-      "mpirun -np $procs /home/us3/bin/us_mpi_analysis $this->tarfile\n";
+      "cd $workdir\n"                                 .
+      "mpirun -np $procs /home/us3/bin/us_mpi_analysis $tarfile\n";
 
       $this->data[ 'pbsfile' ] = $contents;
 
@@ -145,14 +136,25 @@ $this->message[] = "Files copied";
    {
       date_default_timezone_set( "America/Chicago" );
  
-      $cmd   = "ssh -p $this->port -x us3@$this->address qsub $this->workdir/us3.pbs 2>&1";
-$this->message[] = "cmd: $cmd\n";
+      $cluster   = $this->data[ 'job' ][ 'cluster_shortname' ];
+      $requestID = $this->data[ 'job' ][ 'requestID' ];
+      $jobid     = $this->data[ 'db' ][ 'name' ] . sprintf( "-%06d", $requestID );
+      $workdir   = $this->grid[ $cluster ][ 'workdir' ] . $jobid;
+      $address   = $this->grid[ $cluster ][ 'name' ];
+      $port      = $this->grid[ $cluster ][ 'sshport' ]; 
+      $tarfile   = sprintf( "hpcinput-%s-%s-%05d.tar",
+                         $this->data['db']['host'],
+                         $this->data['db']['name'],
+                         $this->data['job']['requestID'] );
+
+      // Submit job to the queue
+      $cmd   = "ssh -p $port -x us3@$address qsub $workdir/us3.pbs 2>&1";
       $jobid = exec( $cmd, $output, $status );
-$this->message[] = "cmd output: " . implode( "\n", $output ) . "\n";
+
+      // Save the job ID
       $this->data[ 'eprfile' ] = rtrim( $jobid );;
 
-$this->message[] = "Job submitted";
-$this->message[] = "Job ID:" . $this->data[ 'eprfile' ];
+$this->message[] = "Job submitted; ID:" . $this->data[ 'eprfile' ];
    }
  
    function update_db()
@@ -167,70 +169,71 @@ $this->message[] = "Job ID:" . $this->data[ 'eprfile' ];
       global $dbhost;
       global $dbname;
 
-      $db = mysql_connect( $dbhost, $dbusername, $dbpasswd );
+      $cluster   = $this->data['job']['cluster_shortname'];
+      $pbs       = mysql_real_escape_string( $this->data[ 'pbsfile' ] );
+      $requestID = $this->data[ 'job' ][ 'requestID' ];
+      $eprfile   = $this->data['eprfile'];
+
+      $link = mysql_connect( $dbhost, $dbusername, $dbpasswd );
  
-      if ( ! $db )
+      if ( ! $link )
       {
          $this->message[] = "Cannot open database on $dbhost\n";
          return;
       }
  
-      if ( ! mysql_select_db( $dbname, $db ) ) 
+      if ( ! mysql_select_db( $dbname, $link ) ) 
       {
          $this->message[] = "Cannot change to database $dbname\n";
          return;
       }
  
-      $pbs = mysql_real_escape_string( $this->data[ 'pbsfile' ] );
-
-      $query = "INSERT INTO HPCAnalysisResult SET "        .
-               "HPCAnalysisRequestID='$this->requestID', " .
-               "jobfile='$pbs', "                          .
-               "gfacID='" . $this->data[ 'eprfile' ]      . "'";
+      $query = "INSERT INTO HPCAnalysisResult SET "  .
+               "HPCAnalysisRequestID='$requestID', " .
+               "jobfile='$pbs', "                    .
+               "gfacID='$eprfile' ";
       
-      $result = mysql_query( $query, $db );
+      $result = mysql_query( $query, $link );
  
       if ( ! $result )
       {
-         $this->message[] = "Invalid query:\n$query\n" . mysql_error( $db ) . "\n";
+         $this->message[] = "Invalid query:\n$query\n" . mysql_error( $link ) . "\n";
          return;
       }
  
-      mysql_close( $db );
+      mysql_close( $link );
 
       // Insert initial data into global DB
-      $db = mysql_connect( $globaldbhost, $globaldbuser, $globaldbpasswd );
+      $gfac_link = mysql_connect( $globaldbhost, $globaldbuser, $globaldbpasswd );
  
-      if ( ! $db )
+      if ( ! $gfac_link )
       {
          $this->message[] = "Cannot open database on $globaldbhost\n";
          return;
       }
  
-      if ( ! mysql_select_db( $globaldbname, $db ) ) 
+      if ( ! mysql_select_db( $globaldbname, $gfac_link ) ) 
       {
          $this->message[] = "Cannot change to database $globaldbname\n";
          return;
       }
- 
-      $pbs = mysql_real_escape_string( $this->data[ 'pbsfile' ] );
 
-      $query = "INSERT INTO analysis SET "                    .
-               "gfacID='"  . $this->data[ 'eprfile' ] . "', " .
-               "cluster='" . $this->cluster           . "', " .
+      $query = "INSERT INTO analysis SET " .
+               "gfacID='$eprfile', "       .
+               "cluster='$cluster', "      .
                "us3_db='$dbname'";
 
-      $result = mysql_query( $query, $db );
+      $result = mysql_query( $query, $gfac_link );
  
       if ( ! $result )
       {
-         $this->message[] = "Invalid query:\n$query\n" . mysql_error( $db ) . "\n";
+         $this->message[] = "Invalid query:\n$query\n" . mysql_error( $gfac_link ) . "\n";
          return;
       }
  
-      mysql_close( $db );
+      mysql_close( $gfac_link );
 
-$this->message[] = "Database $dbname updated: requestID = $this->requestID";
+$this->message[] = "Database $dbname updated: requestID = $requestID";
    }
 }
 ?>
