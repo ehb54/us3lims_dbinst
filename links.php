6fb802f45c19b8b0e1c5b36d878ba6ec81b480a6
zollarsd <zollarsd@bf3aa11d-60f9-42ff-b629-36e157aa8545>
2012-09-27 15:41:47 +0000

    Added bcf cert authority

--- links.php
+++ links.php
@@ -11,47 +11,49 @@ $userlevel = ( isset( $_SESSION['userlevel'] ) ) ? $_SESSION['userlevel'] : -1;
 
 $projects_menu = <<<HTML
   <h4>Project</h4>
-  <a href='view_projects.php'>Projects</a>
-  <a href='edit_images.php'>Images</a>
-  <a href='view_reports.php'>Reports</a>
-  <a href='data_sharing.php'>Sharing</a>
+  <a href='https://$org_site/view_projects.php'>Projects</a>
+  <a href='https://$org_site/edit_images.php'>Images</a>
+  <a href='https://$org_site/view_reports.php'>Reports</a>
+  <a href='https://$org_site/data_sharing.php'>Sharing</a>
 
 HTML;
 
 $analysis_menu = <<<HTML
   <h4>Analysis</h4>
-  <a href='queue_setup_1.php'>Queue Setup</a>
-  <a href='2DSA_1.php'>2DSA Analysis</a>
-  <a href='2DSA-CG_1.php'>2DSA Custom Grid</a>
-  <a href='GA_1.php'>GA Analysis</a>
-  <a href='runID_info.php'>RunID Info</a></li>
+  <a href='https://$org_site/queue_setup_1.php'>Queue Setup</a>
+  <a href='https://$org_site/2DSA_1.php'>2DSA Analysis</a>
+  <a href='https://$org_site/2DSA-CG_1.php'>2DSA Custom Grid</a>
+  <a href='https://$org_site/GA_1.php'>GA Analysis</a>
+  <a href='https://$org_site/runID_info.php'>RunID Info</a></li>
 
 HTML;
 
 $monitor_menu = <<<HTML
   <h4>Status Monitor</h4>
-  <a href='queue_viewer.php'>Queue Status</a>
+  <a href='https://$org_site/queue_viewer.php'>Queue Status</a>
   <a href='http://grid.uthscsa.edu'>Cluster Status</a>
 
 HTML;
 
 $general_menu = <<<HTML
   <h4>General</h4>
-  <a href='http://$org_site/profile.php?edit=12'>Change My Info</a>
+  <a href='https://$org_site/profile.php?edit=12'>Change My Info</a>
   <a href='http://$org_site/view_database_info.php'>Database Login Info</a>
   <a href="partners.php">Partners</a>
   <a href='contacts.php'>Contacts</a>
   <a href='mailto:$admin_email'>Webmaster</a>
+  <a href='data_security.php'>Data Security</a>
   <a href='http://$org_site/logout.php'>Logout</a>
 
 HTML;
 
 $general_menu_1 = <<<HTML
   <h4>General</h4>
-  <a href='http://$org_site/profile.php?edit=12'>Change My Info</a>
+  <a href='https://$org_site/profile.php?edit=12'>Change My Info</a>
   <a href="partners.php">Partners</a>
   <a href='contacts.php'>Contacts</a>
   <a href='mailto:$admin_email'>Webmaster</a>
+  <a href='data_security.php'>Data Security</a>
   <a href='http://$org_site/logout.php'>Logout</a>
 
 HTML;
@@ -61,7 +63,7 @@ if ( $userlevel == 5 )  // level 5 = super admin ( developer )
   $sidebar_menu = <<<HTML
   <h4>Navigation</h4>
   <a href="http://$org_site/index.php">Welcome!</a>
-  <a href='http://$org_site/admin_links.php'>Admin Info</a>
+  <a href='https://$org_site/admin_links.php'>Admin Info</a>
   $projects_menu
   $analysis_menu
   $monitor_menu
@@ -75,7 +77,7 @@ else if ( $userlevel == 4 )  // userlevel 4 = admin
   $sidebar_menu = <<<HTML
   <h4>Navigation</h4>
   <a href="http://$org_site/index.php">Welcome!</a>
-  <a href='http://$org_site/admin_links.php'>Admin Info</a>
+  <a href='https://$org_site/admin_links.php'>Admin Info</a>
   $projects_menu
   $analysis_menu
   $monitor_menu
@@ -89,7 +91,7 @@ else if ( $userlevel == 3 )  // userlevel 3 = superuser
   $sidebar_menu = <<<HTML
   <h4>Navigation</h4>
   <a href="http://$org_site/index.php">Welcome!</a>
-  <a href='http://$org_site/admin_links.php'>Admin Info</a>
+  <a href='https://$org_site/admin_links.php'>Admin Info</a>
   $projects_menu
   $analysis_menu
   $monitor_menu
@@ -140,6 +142,7 @@ else // not logged in
   <a href="partners.php">Partners</a>
   <a href='contacts.php'>Contacts</a>
   <a href='mailto:$admin_email'>Webmaster</a>
+  <a href='data_security.php'>Data Security</a>
   <a href='https://$org_site/login.php'>Login</a>
 
 HTML;
