b3080abc7b094a9e96b9aefd9f9d17d4dc4f8777
Gary E. Gorbet <gegorbet@gmail.com>
2013-10-22 20:54:39 +0000

    disallow delete of last instrument record, and remove bcf from cluster list

--- lib/utility.php
+++ lib/utility.php
@@ -77,7 +77,7 @@ $clusters = array(
 ##  new cluster_info( "gordon.sdsc.edu",             "gordon",      "normal"  ), 
   new cluster_info( "juropa.fz-juelich.de",        "juropa",      "default" ), 
   new cluster_info( "alamo.uthscsa.edu",           "alamo",       "default" ),
-  new cluster_info( "bcf.uthscsa.edu",             "bcf",         "default" ),
+##  new cluster_info( "bcf.uthscsa.edu",             "bcf",         "default" ),
   new cluster_info( "alamo.uthscsa.edu",           "alamo-local", "default" ),
   new cluster_info( "bcf.uthscsa.edu",             "bcf-local",   "default" )
   );
