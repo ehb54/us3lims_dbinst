88d4efb0b1fc3f34dbaa987ba626179698c85ce7
Gary E. Gorbet <gegorbet@gmail.com>
2014-01-24 21:23:04 +0000

    Preliminary fix to insure Uniform Grid Repetitions is integer

--- lib/controls.php
+++ lib/controls.php
@@ -308,8 +308,29 @@ function uniform_grid_setup()
 {
 echo<<<HTML
     <fieldset class='option_value'>
-      <legend>Uniform Grid Repetitions Setup</legend>
-      <input type="text" value="6" name="uniform_grid"/> Uniform Grid Repetitions<br/>
+      <legend>Uniform Grid Repetitions</legend>
+      <select name="uniform grid" id='uniform_grid'>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6' selected='selected'>6</option>
+        <option value='7'>7</option>
+        <option value='8'>8</option>
+        <option value='9'>9</option>
+        <option value='10'>10</option>
+        <option value='11'>11</option>
+        <option value='12'>12</option>
+        <option value='13'>13</option>
+        <option value='14'>14</option>
+        <option value='15'>15</option>
+        <option value='16'>16</option>
+        <option value='17'>17</option>
+        <option value='18'>18</option>
+        <option value='19'>19</option>
+        <option value='20'>20</option>
+      </select>
     </fieldset>
 HTML;
 }
