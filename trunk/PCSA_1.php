f77bc1334b4939a363144b8bf370df1f373c0fc1
gegorbet <gegorbet@bf3aa11d-60f9-42ff-b629-36e157aa8545>
2014-12-04 01:09:16 +0000

    add in PCSA php scripts

---
+++ trunk/PCSA_1.php
@@ -0,0 +1,268 @@
+<?php
+/*
+ * PCSA_1.php
+ *
+ * A place to start entering submission parameters for the PCSA analysis
+ *
+ */
+include_once 'checkinstance.php';
+
+if ( ($_SESSION['userlevel'] != 2) &&
+     ($_SESSION['userlevel'] != 3) &&
+     ($_SESSION['userlevel'] != 4) &&
+     ($_SESSION['userlevel'] != 5) )    // only data analyst and up
+{
+  header('Location: index.php');
+  exit();
+} 
+
+// Verify that job submission is ok now
+include 'lib/motd.php';
+if ( motd_isblocked() && ($_SESSION['userlevel'] < 4) )
+{
+  header("Location: index.php");
+  exit();
+}
+
+// Verify that there is something in the queue
+if ( ! isset( $_SESSION['request'] ) || sizeof( $_SESSION['request'] ) < 1 )
+{
+  header("Location: queue_setup_1.php");
+  exit();
+}
+
+// define( 'DEBUG', true );
+
+include 'config.php';
+include 'db.php';
+include 'lib/utility.php';
+include 'lib/payload_manager.php';
+include 'lib/controls.php';
+
+// Make sure the advancement level is set
+$advanceLevel = ( isset($_SESSION['advancelevel']) )
+              ? $_SESSION['advancelevel'] : 0;
+
+$separate_datasets = ( isset( $_SESSION['separate_datasets'] ) )
+                   ? $_SESSION['separate_datasets'] : 1;
+$advanced_review   = ( isset( $_SESSION['advanced_review'  ] ) )
+                   ? $_SESSION['advanced_review'  ] : 0;
+
+// To support multiple datasets, let's keep track of which one we're on
+$num_datasets = sizeof( $_SESSION['request'] );
+
+// Create the payload manager
+$payload  = new Payload_PCSA( $_SESSION );
+
+// First, let's see if the "TIGRE" button has been pressed
+if ( isset($_POST['TIGRE']) )
+{
+  $dataset_id = $num_datasets - 1;
+
+  // Save cluster information
+  if ( isset($_POST['cluster']) )
+  {
+    list( $cluster_name, $cluster_shortname, $queue ) = explode(":", $_POST['cluster'] );
+    $_SESSION['cluster']              = array();
+    $_SESSION['cluster']['name']      = $cluster_name;
+    $_SESSION['cluster']['shortname'] = $cluster_shortname;
+    if ( $cluster_shortname == 'alamo' )
+    {
+      if ( isset($_SESSION['separate_datasets']) )
+      {
+        if ( $_SESSION['separate_datasets'] == 0 )
+        {
+          $queue = 'ngenseq';
+        }
+      }
+
+      if ( $num_datasets > 4 )
+        $queue = 'ngenseq';
+    }
+    $_SESSION['cluster']['queue']     = $queue;
+  }
+
+  if ( $num_datasets > 1  &&  $advanced_review == 0 )
+  { // For multiple datasets and no advanced review, create all payloads
+    $payload->clear();
+    for ( $jdataset_id = 0; $jdataset_id < ( $num_datasets - 1 ); $jdataset_id++ )
+    {
+      $payload->acquirePostedData( $jdataset_id, $num_datasets );
+    }
+    $payload->save();
+  }
+
+  // get previous payload data and add this session to it
+  $payload->restore();
+  $payload->add( 'cluster', $_SESSION['cluster'] );
+  $payload->acquirePostedData( $dataset_id, $num_datasets );
+  $payload->save();
+
+  // Check to see if the file is too big
+  if ( $advanceLevel == 0 )
+    ; //    check_filesize();
+
+//  $payload->show();
+  header("Location: PCSA_2.php");
+  exit();
+}
+
+// Now let's see if the "next" button has been pressed
+else if ( isset($_POST['next']) )
+{
+  $dataset_id = ( $_POST['dataset_id'] < $num_datasets - 1 )
+                ? $_POST['dataset_id'] : $num_datasets - 1;
+
+  // get previous payload data and add this session to it
+  $payload->restore();
+  $payload->acquirePostedData( $dataset_id, $num_datasets );
+  $payload->save();
+
+  $dataset_id++;
+}
+
+// In this case it's the first time here
+else
+{
+  $dataset_id = 0;
+
+  // Add the initial options to the payload
+  $payload->clear();
+
+  $payload->save();
+
+  // If multi-data and no advanced review, point to last dataset
+  if ( $advanced_review == 0  &&
+       $num_datasets > 1 )
+    $dataset_id = $num_datasets - 1;
+}
+
+// Get some other session data
+$editMeniscus = $_SESSION['request'][$dataset_id]['editMeniscus'];
+$dataLeft     = $_SESSION['request'][$dataset_id]['dataLeft'];
+
+// Start displaying page
+$page_title = 'PCSA Analysis';
+$css = 'css/slider.css';
+$js = 'js/analysis.js,js/PCSA.js';
+include 'header.php';
+
+?>
+<!-- Begin page content -->
+<div id='content'>
+
+  <h1 class="title"><?php echo $page_title; ?></h1>
+  <!-- Place page content here -->
+
+<div>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"
+      onsubmit="return validate(this, 
+                <?php echo $advanceLevel; ?>, 
+                <?php echo $dataset_id; ?>,
+                <?php echo $num_datasets; ?>, 
+                <?php echo $separate_datasets; ?>,
+                <?php echo $editMeniscus; ?>,
+                <?php echo $dataLeft; ?>);" >
+
+
+<?php
+// if ( isset($error) ) echo $error;
+
+  $max_dset_id  = $num_datasets - 1;
+  $dataset_id   = min( $dataset_id, $max_dset_id );
+  $display_dsid = ( $advanced_review == 0 ) ? 0 : $dataset_id;
+
+  display( $display_dsid, $num_datasets );
+
+  // Display some information about the current dataset
+  echo "  <fieldset>\n" .
+       "    <h3>Dataset control:</h3>\n" .
+       "    <ul>\n" .
+       "      <li>Current dataset number: " . ($dataset_id + 1) . "</li>\n" .
+       "      <li>Run Name: {$_SESSION['request'][$dataset_id]['filename']}</li>\n" .
+       "      <li>Number of datasets: $num_datasets</li>\n" .
+       "    </ul>\n";
+
+  // Add some controls to move to the next dataset, if there's more than one
+  if ( $num_datasets > 1 && $dataset_id < $max_dset_id )
+  {
+    echo "    <input type='hidden' name='dataset_id' value='$dataset_id' />\n" .
+         "    <input class='submit' type='submit' name='next' value='Next Dataset --&gt;' /></p>\n";
+  }
+
+  echo "  </fieldset>\n";
+
+  if ( $dataset_id == $max_dset_id )
+  {
+    echo tigre();
+  }
+
+?>
+
+</form>
+<?php $payload->show(); ?>
+
+</div>
+
+</div>
+
+<?php
+include 'footer.php';
+exit();
+
+// A function to display controls for one dataset
+function display( $dataset_id, $num_datasets )
+{
+  // Get edited data profile
+  $parts = explode( ".", $_SESSION['request'][$dataset_id]['editFilename'] );
+  $edit_text = $parts[1];
+  echo "  <fieldset>" .
+       "    <legend>Initialize PCSA Parameters - " .
+       "{$_SESSION['request'][$dataset_id]['filename']}; " .
+       "Edit profile: $edit_text; " .
+       "            Dataset " . ($dataset_id + 1) . " of $num_datasets</legend>\n";
+
+  if ( $dataset_id == 0 )
+  {
+    pcsa_pars_setup();
+    montecarlo();
+    if ( $num_datasets > 1 )
+    {
+      PMGC_option();
+    }
+    tinoise_option();
+  }
+ 
+  echo<<<HTML
+    <p><button onclick="return toggle('advanced');" id='show'>
+      Show Advanced Options</button></p>
+
+    <div id='advanced' style='display:none;'>
+
+HTML;
+
+  if ( $dataset_id == 0 )
+  {
+    rinoise_option();
+    debug_option();
+  }
+
+  simpoints_input();
+  band_volume_input();
+  radial_grid_input();
+  time_grid_input();
+
+  echo<<<HTML
+    </div>
+
+    <input class="submit" type="button" 
+            onclick='window.location="queue_setup_2.php"' 
+            value="Edit Profiles"/>
+    <input class="submit" type="button" 
+            onclick='window.location="queue_setup_1.php"' 
+            value="Change Experiment"/>
+  </fieldset>
+HTML;
+  }
+
+?>
---
+++ trunk/js/PCSA.js
@@ -0,0 +1,554 @@
+// Javascript for PCSA controls
+
+// jQuery slider controls
+$(document).ready(function()
+{
+  // Montecarlo Slider setup
+  $("#montecarlo-min").attr('value', 1  );
+  $("#montecarlo-max").attr('value', 100);
+  $("#montecarlo-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   1,
+    min:     1,
+    max:     100,
+    step:    1,
+    slide: function( event, ui )
+    {
+      $( "#mc_iterations" ).attr('value', ui.value);
+    },
+  });
+
+  $("#mc_iterations").change( function()
+  {
+    $("#montecarlo-slider").slider( 'value', this.value );
+  });
+
+  // Main var counts setup
+  $("#varcount-min").attr('value', 2  );
+  $("#varcount-max").attr('value', 20 );
+  $("#varcount-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   10,
+    min:     2,
+    max:     20,
+    step:    1,
+    slide: function( event, ui )
+    {
+      $( "#varcount_points" ).attr('value', ui.value);
+    },
+  });
+
+  $("#varcount_points").change( function()
+  {
+    $("#varcount-slider").slider( 'value', this.value );
+  });
+
+  // Horizontal-Line var counts setup
+  $("#hlvcount-min").attr('value', 20  );
+  $("#hlvcount-max").attr('value', 400 );
+  $("#hlvcount-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   100,
+    min:     20,
+    max:     400,
+    step:    1,
+    slide: function( event, ui )
+    {
+      $( "#hlvcount_points" ).attr('value', ui.value);
+    },
+  });
+
+  $("#hlvcount_points").change( function()
+  {
+    $("#hlvcount-slider").slider( 'value', this.value );
+  });
+
+  // Grid-Fit Iterations Slider setup
+  $("#gfititer-min").attr('value', 1  );
+  $("#gfititer-max").attr('value', 10 );
+  $("#gfititer-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   3,
+    min:     1,
+    max:     10,
+    step:    1,
+    slide: function( event, ui )
+    {
+      $( "#gfit_iterations" ).attr('value', ui.value);
+    },
+  });
+
+  $("#gfit_iterations").change( function()
+  {
+    $("#gfititer-slider").slider( 'value', this.value );
+  });
+
+  // Curves Points Slider setup
+  $("#curvpoint-min").attr('value', 50  );
+  $("#curvpoint-max").attr('value', 500);
+  $("#curvpoint-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   200,
+    min:     50,
+    max:     1000,
+    step:    1,
+    slide: function( event, ui )
+    {
+      $( "#curves_points" ).attr('value', ui.value);
+    },
+  });
+
+  $("#curves_points").change( function()
+  {
+    $("#curvpoint-slider").slider( 'value', this.value );
+  });
+
+  // Simpoints Slider setup
+  $("#simpoints-min").attr('value', 50  );
+  $("#simpoints-max").attr('value', 5000);
+  $("#simpoints-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   200,
+    min:     50,
+    max:     5000,
+    step:    1,
+    slide: function( event, ui )
+    {
+      $( "#simpoints-value" ).attr('value', ui.value);
+    },
+  });
+
+  $("#simpoints-value").change( function()
+  {
+    $("#simpoints-slider").slider( 'value', this.value );
+  });
+
+  // Band_volume Slider setup
+  $("#band_volume-min").attr('value', 0.0 );
+  $("#band_volume-max").attr('value', 0.05);
+  $("#band_volume-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   0.015,
+    min:     0.0,
+    max:     0.05,
+    step:    0.0001,
+    slide: function( event, ui )
+    {
+      $( "#band_volume-value" ).attr('value', ui.value);
+    },
+  });
+
+  $("#band_volume-value").change( function()
+  {
+    $("#band_volume-slider").slider( 'value', this.value );
+  });
+
+  // Parallel masters group count setup
+  $("#PMGC_enable").change( function()
+  {
+    if ( $("#PMGC_enable").is(":checked") )
+    {
+       $("#PMGC_count").show();
+       $("#req_mgroupcount").attr( 'value', 8 );
+    }
+
+    else
+    {
+       $("#PMGC_count").hide();
+       $("#req_mgroupcount").attr( 'value', 1 );
+    }
+
+  });
+
+  // Debug_level Slider setup
+  $("#debug_level-min").attr('value', 0 );
+  $("#debug_level-max").attr('value', 4 );
+  $("#debug_level-slider").slider(
+  {
+    animate: true,
+    range:   "min",
+    value:   0,
+    min:     0,
+    max:     4,
+    step:    1,
+    slide: function( event, ui )
+    {
+      $( "#debug_level-value" ).attr('value', ui.value);
+    },
+  });
+
+  $("#debug_level-value").change( function()
+  {
+    $("#debug_level-slider").slider( 'value', this.value );
+  });
+
+});
+
+function show_ctl(num) 
+{
+  var which = document.getElementById('mag'+num);
+  which.style.display = 'block';
+}
+
+function hide(num) 
+{
+  var which = document.getElementById('mag'+num);
+  which.style.display='none';
+}
+
+function show_hide( value, num1, num2 )
+{
+  var shdoc = document.getElementById('mag'+num1);
+  var hidoc = document.getElementById('mag'+num2);
+  if ( value != 'HL' )
+  {
+    shdoc.style.display = 'block';
+    hidoc.style.display = 'none';
+  }
+  else
+  {
+    shdoc.style.display = 'none';
+    hidoc.style.display = 'block';
+  }
+}
+
+function toggle(area)
+{
+  var which = document.getElementById(area);
+  var text  = document.getElementById('show'); 
+
+  if ( which.style.display == 'block' ) 
+  {  
+    if ( document.all )  // old IE
+      text.innerHTML = "Show Advanced Options";
+    else
+      text.textContent = "Show Advanced Options";
+    which.style.display = 'none';
+  }
+  else
+  {
+    if ( document.all )  // old IE
+      text.innerHTML = "Hide Advanced Options";
+    else
+      text.textContent = "Hide Advanced Options";
+    which.style.display = 'block';
+  }
+
+  return false;
+}
+
+function validate( f, advanceLevel, dataset_num, count_datasets, separate_datasets,
+                      meniscus_radius, data_left )
+{
+  // Advanced users don't go through these tests
+  if ( advanceLevel > 0 ) return( true );
+
+  // None of the controls we're checking exist after the first dataset
+  if ( dataset_num > 0 ) return( true );
+
+  // Handling could be different depending on single or multiple datasets
+  // First, checks that are common to both
+
+  // Make sure there is one on the page
+  if ( valid_field(f.s_value_min) )
+  {
+    var s_value_min = parseFloat(f.s_value_min.value);
+    var s_value_max = parseFloat(f.s_value_max.value);
+
+    // alert("s_value_min = " + s_value_min +
+    //       "\ns_value_max = " + s_value_max);
+
+    if ( s_value_max < s_value_min )
+    {
+      var swap = confirm( "The maximum s-value is less than the minimum " +
+                          "s-value. If you would like to switch them, " +
+                          "please click on OK to continue. If you would " +
+                          "rather edit them yourself, click cancel to " +
+                          "return to the submission form." );
+
+      if ( ! swap ) return( false );
+
+      f.s_value_min.value = s_value_max;
+      f.s_value_max.value = s_value_min;
+    }
+
+    s_value_min         = parseFloat(f.s_value_min.value);  // These might have changed
+    s_value_max         = parseFloat(f.s_value_max.value);
+
+    // What if the s_values are entirely inside the -0.1 ~ +0.1 range?
+    if ( (s_value_min >= -0.1) && (s_value_max <=  0.1) )
+    {
+      alert( "Your s-value range is entirely within the -0.1 ~ 0.1 range. " +
+             "This range is not allowed." );
+      return( false );
+    }
+
+    // Some of the s_value range should be outside the - 0.1 ~ + 0.1 range
+    if ( (s_value_min <= -0.1) && (s_value_max >= -0.1) ||
+         (s_value_min <=  0.1) && (s_value_max >=  0.1) )
+    {
+      var answer = confirm( "Your s-value range overlaps the -0.1 ~ 0.1 range. " +
+                            "This range will be excluded from the 2DSA analysis." +
+                            "Please click OK to continue, or click cancel to " +
+                            "return to the submission form." );
+
+      if ( ! answer ) return( false );
+    }
+  }
+  
+  // Verify these fields exist
+  if ( valid_field(f.mw_value_min) )
+  {
+    var mw_value_min = parseFloat(f.mw_value_min.value);
+    var mw_value_max = parseFloat(f.mw_value_max.value);
+
+    // alert("mw_value_min = " + mw_value_min +
+    //       "\nmw_value_max = " + mw_value_max);
+
+    if ( mw_value_max < mw_value_min )
+    {
+      var swap = confirm( "The maximum mw-value is less than the minimum " +
+                          "mw-value. If you would like to switch them, " +
+                          "please click on OK to continue. If you would " +
+                          "rather edit them yourself, click cancel to " +
+                          "return to the submission form." );
+
+      if ( ! swap ) return( false );
+
+      f.mw_value_min.value = mw_value_max;
+      f.mw_value_max.value = mw_value_min;
+    }
+  }
+
+  if ( valid_field(f.ff0_min) )
+  {
+    var ff0_min = parseFloat(f.ff0_min.value);
+    var ff0_max = parseFloat(f.ff0_max.value);
+
+    // alert("ff0_min = " + ff0_min +
+    //       "\nff0_max = " + ff0_max);
+
+    if ( ff0_max < ff0_min )
+    {
+      var swap = confirm( "The maximum f/f0-value is less than the minimum " +
+                          "f/f0-value. If you would like to switch them, " +
+                          "please click on OK to continue. If you would " +
+                          "rather edit them yourself, click cancel to " +
+                          "return to the submission form." );
+
+      if ( ! swap ) return( false );
+
+      f.ff0_min.value = ff0_max;
+      f.ff0_max.value = ff0_min;
+    }
+  }
+
+  // What if the user requests meniscus fitting but the fit range extends into 
+  // the data?
+  var meniscus_option = 0;
+  if ( valid_field(f.meniscus_option) )
+    meniscus_option = parseInt( get_radio_value(f.meniscus_option) );
+
+  if ( meniscus_option == 1 )
+  {
+    var meniscus_range = parseFloat( f.meniscus_range.value );
+
+    // alert( "meniscus range = " + meniscus_range +
+    //        "\nmeniscus radius = " + meniscus_radius +
+    //        "\nleftmost data point = " + data_left );
+
+    var range_limit = data_left - meniscus_radius - 0.002; // a fudge factor
+    range_limit = Math.round( range_limit * 2000 ) / 1000; // multiply by 2 and round to 3 digits
+    if ( meniscus_range > range_limit )
+    {
+       f.meniscus_range.value = range_limit;
+       alert( "The meniscus fit range extends beyond the left data range limit. " +
+              "The meniscus range has been reduced to " + range_limit );
+    }
+  }
+
+  else
+  {  // If not fitting meniscus, just be sure meniscus below data range
+    f.meniscus_range.value = 0.001;
+
+    if ( meniscus_radius >= data_left )
+    {
+      alert( "The data's meniscus value extends beyond the left data range limit." );
+      return( false );
+    }
+  }
+
+  if ( count_datasets == 1 || separate_datasets == 1 )
+    return( validate_single(f) );
+
+  else
+    return( validate_multiple(f) );
+
+  return( true );
+}
+
+function validate_single( f )
+{    
+  var contact_bo = "\nIf you have any questions about this policy, please " +
+                   "contact Borries Demeler (demeler@biochem.uthscsa.edu).";
+
+  // Some things should only be done if monte carlo is 1
+  var monte_carlo = 1;
+  if ( valid_field(f.mc_iterations) )
+    monte_carlo = parseInt( f.mc_iterations.value );
+
+  // alert( "monte_carlo value = " + monte_carlo );
+
+  if ( monte_carlo > 1 )
+  {
+    // Meniscus fitting
+    var meniscus_option = 0;
+    if ( valid_field(f.meniscus_option) )
+      meniscus_option = parseInt( get_radio_value(f.meniscus_option) );
+
+    // alert( "meniscus_option = " + meniscus_option );
+
+    if ( meniscus_option == 1 )
+    {
+      alert( "Meniscus fitting may only be performed when no Monte Carlo " +
+             "iterations are requested.\n" + contact_bo );
+      return( false );
+    }
+
+    // Iterative fitting
+    var iterations_option = 0;
+    if ( valid_field(f.iterations_option) )
+      iterations_option = parseInt( get_radio_value(f.iterations_option) );
+    // alert( "iterations_option = " + iterations_option );
+
+    var ti_noise = 0;
+    if ( valid_field(f.tinoise_option) )
+      ti_noise = parseInt( get_radio_value(f.tinoise_option) );
+
+    var ri_noise = 0;
+    if ( valid_field(f.rinoise_option) )
+      ri_noise = parseInt( get_radio_value(f.rinoise_option) );
+    // alert("tinoise_option value = " + ti_noise +
+    //       "\nrinoise_option value = " + ri_noise);
+
+    if ( ti_noise == 1 || ri_noise == 1 )
+    {
+      alert( "Time Invariant Noise subtraction and Radially Invariant Noise " +
+             "subtraction may only be performed when no monte carlo " +
+             "iterations are requested.\n" + contact_bo );
+      return( false );
+    }
+  }
+
+  else
+  {
+    // Otherwise ti noise subtraction should be performed
+    var ti_noise = 0;
+    if ( valid_field(f.tinoise_option) )
+      ti_noise = parseInt( get_radio_value(f.tinoise_option) );
+
+    // alert("tinoise_option value = " + ti_noise);
+
+    var tinoise_ok = ( ti_noise == 1 ) ||
+                     confirm( "If ti noise subtraction has already been performed " +
+                              "on this data, click OK to continue. If not, click " +
+                              "cancel and turn on \"Fit Time Invariant Noise.\"\n" +
+                              contact_bo );
+
+    if ( ! tinoise_ok ) return( false );
+  }
+
+  return( true );
+}
+
+function validate_multiple( f )
+{
+  var contact_bo = "\nIf you have any questions about this policy, please " +
+                   "contact Borries Demeler (demeler@biochem.uthscsa.edu).";
+
+  // For global analyses ti and ri noise subtraction should not be checked
+  var ti_noise = 0;
+  if ( valid_field(f.tinoise_option) )
+    ti_noise = parseInt( get_radio_value(f.tinoise_option) );
+
+  var ri_noise = 0;
+  if ( valid_field(f.rinoise_option) )
+    ri_noise = parseInt( get_radio_value(f.rinoise_option) );
+
+  // alert("tinoise_option value = " + ti_noise +
+  //       "\nrinoise_option value = " + ri_noise);
+
+  if ( ti_noise == 1 || ri_noise == 1 )
+  {
+    alert( "Time Invariant Noise and Radially Invariant Noise fitting should " +
+           "not be requested on a global analysis.\n" +
+           contact_bo );
+    return( false );
+  }
+
+  // No meniscus fitting in global analyses
+  var meniscus_option = 0;
+  if ( valid_field(f.meniscus_option) )
+    meniscus_option = parseInt( get_radio_value(f.meniscus_option) );
+
+  // alert("meniscus_option = " + meniscus_option);
+
+  if ( meniscus_option == 1 )
+  {
+    alert( "Meniscus fitting is not allowed on multiple datasets.\n" +
+           contact_bo);
+    return( false );
+  }
+
+  // No iterative fitting in global analyses
+  var iterations_option = 0;
+  if ( valid_field(f.iterations_option) )
+    iterations_option = parseInt( get_radio_value(f.iterations_option) );
+  var monte_carlo = 1;
+  if ( valid_field(f.mc_iterations) )
+    monte_carlo = parseInt( f.mc_iterations.value );
+
+  // alert("iterations_option = " + iterations_option);
+  // alert( "monte_carlo value = " + monte_carlo );
+
+  if ( iterations_option == 1  &&  monte_carlo > 1 )
+  {
+    alert( "Iterative fitting is not allowed for Monte Carlo on multiple datasets.\n" +
+           contact_bo );
+    return( false );
+  }
+
+  // Let's only produce this message the first time. On subsequent pages
+  // most of the controls are absent, so...
+  if ( valid_field(f.tinoise_option) )
+  {
+    var multiple_ok = confirm( "You have selected more than one dataset " +
+                      "to be fitted in this analysis. Are you sure you want " +
+                      "to perform a global analysis on all included datasets? " +
+                      "The fitted model will be a compromise between all " +
+                      "included datasets and not return the best possible fit " +
+                      "for each individual dataset. This will also significantly " +
+                      "increase the computing time. If this is not what you want " +
+                      "to do, please click on cancel and go back to the dataset " +
+                      "selection and delete the extra datasets from the queue. " +
+                      "Otherwise, select OK to continue.");
+
+    if ( ! multiple_ok ) return( false );
+  }
+
+  return( true );
+}
