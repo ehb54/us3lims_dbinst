a19aca2969a4566f8481917b17af46bc9915ab9a
Gary E. Gorbet <gegorbet@gmail.com>
2014-12-04 01:09:16 +0000

    add in PCSA php scripts

---
+++ PCSA_2.php
@@ -0,0 +1,230 @@
+<?php
+/*
+ * PCSA_2.php
+ *
+ * Final database update and submission for the PCSA analysis
+ *
+ */
+include_once 'checkinstance.php';
+
+if ( ($_SESSION['userlevel'] != 2) &&
+     ($_SESSION['userlevel'] != 3) &&
+     ($_SESSION['userlevel'] != 4) &&
+     ($_SESSION['userlevel'] != 5) )    // only data analyst and up
+{
+  header('Location: index.php');
+  exit();
+} 
+
+// Verify that job submission is ok now
+include 'lib/motd.php';
+if ( motd_isblocked() && ($_SESSION['userlevel'] < 4) )
+{
+  header("Location: index.php");
+  exit();
+}
+
+// define( 'DEBUG', true );
+
+include 'config.php';
+include 'db.php';
+include 'lib/utility.php';
+include 'lib/payload_manager.php';
+include 'lib/HPC_analysis.php';
+include 'lib/file_writer.php';
+include $class_dir . 'submit_local.php';
+include $class_dir . 'submit_gfac.php';
+include $class_dir . 'submit_airavata.php';
+
+global $uses_airavata;
+
+// Create the payload manager and restore the data
+$payload = new Payload_PCSA( $_SESSION );
+$payload->restore();
+
+// Create the HPC analysis agent and file writer
+$HPC       = new HPC_PCSA();
+$file      = new File_PCSA();
+$filenames = array();
+$HPCAnalysisRequestID = 0;
+
+$files_ok  = true;  // Let's also make sure there weren't any problems writing the files
+
+if ( $_SESSION['separate_datasets'] )
+{ // Multiple datasets and non-global: build composite jobs
+  $dataset_count = $payload->get( 'datasetCount' );
+  $job_params    = $payload->get( 'job_parameters' );
+  $mgroup_count  = max( 1, $job_params['req_mgroupcount'] );
+  $mc_iters      = max( 1, $job_params['mc_iterations'] );
+  $reqds_count   = 50;              // Initial datasets per request
+  if ( $mc_iters > 50 )
+    $reqds_count   = 25;
+  $groups        = (int)( $reqds_count / $mgroup_count );
+  $groups        = max( 1, $groups );
+  $reqds_count   = $mgroup_count * $groups;  // Multiple of PMGC
+  $ds_remain     = $dataset_count;  // Remaining datasets
+  $index         = 0;               // Input datasets index
+  $kr            = 0;               // Output request index
+
+  while ( $ds_remain > 0 )
+  { // Loop to build HPC requests of composite jobs
+    if ( ( $ds_remain - $reqds_count ) < $mgroup_count )
+      $reqds_count   = $ds_remain;
+    else
+      $reqds_count   = min( $reqds_count, $ds_remain );
+
+    $composite     = $payload->get_ds_range( $index, $reqds_count );
+    $HPCAnalysisRequestID = $HPC->writeDB( $composite );
+    $filenames[ $kr ] = $file->write( $composite, $HPCAnalysisRequestID );
+    if ( $filenames[ $kr ] === false )
+      $files_ok = false;
+
+    else
+    { // Write the xml file content to the db
+      $xml_content = mysql_real_escape_string( file_get_contents( $filenames[ $kr ] ) );
+      $edit_filename = $composite['dataset'][0]['edit'];
+
+      $query  = "UPDATE HPCAnalysisRequest " .
+                "SET requestXMLfile = '$xml_content', " .
+                "editXMLFilename = '$edit_filename' " .
+                "WHERE HPCAnalysisRequestID = $HPCAnalysisRequestID ";
+      mysql_query( $query )
+            or die("Query failed : $query<br />\n" . mysql_error());
+    }
+
+    $index        += $reqds_count;
+    $ds_remain    -= $reqds_count;
+    $kr++;
+  }
+}
+
+else
+{ // Multiple datasets and global
+  $globalfit = $payload->get();
+  $HPCAnalysisRequestID = $HPC->writeDB( $globalfit );
+  $filenames[ 0 ] = $file->write( $globalfit, $HPCAnalysisRequestID );
+  if ( $filenames[ 0 ] === false )
+    $files_ok = false;
+
+  else
+  {
+    // Write the xml file content to the db
+    $xml_content = mysql_real_escape_string( file_get_contents( $filenames[ 0 ] ) );
+    $edit_filename = $globalfit['dataset'][0]['edit'];
+
+    $query  = "UPDATE HPCAnalysisRequest " .
+              "SET requestXMLfile = '$xml_content', " .
+              "editXMLFilename = '$edit_filename' " .
+              "WHERE HPCAnalysisRequestID = $HPCAnalysisRequestID ";
+    mysql_query( $query )
+          or die("Query failed : $query<br />\n" . mysql_error());
+  }
+}
+
+if ( $files_ok )
+{
+  $output_msg = <<<HTML
+  <pre>
+  Thank you, your job was accepted and is currently processing. An
+  email will be sent to {$_SESSION['submitter_email']} when the job is
+  completed.
+
+HTML;
+
+  // EXEC COMMAND FOR TIGRE 
+  if ( isset($_SESSION['cluster']) )
+  {
+    $cluster = $_SESSION['cluster']['shortname'];
+    unset( $_SESSION['cluster'] );
+
+    // Currently we are supporting two submission methods.
+    switch ( $cluster )
+    {
+       case 'bcf-local'   :
+       case 'alamo-local' :
+          $job = new submit_local();
+          break;
+       case 'juropa'   : 
+          $job = new submit_gfac();
+          break;
+       case 'stampede' :
+       case 'lonestar' :
+       case 'gordon'   :
+       case 'alamo'    :
+       case 'bcf'      :
+       case 'trestles' :
+          if ( $uses_airavata === true )
+             $job = new submit_airavata();
+          else
+             $job = new submit_gfac();
+          break;
+
+       default         :
+          $output_msg .= "<br /><span class='message'>Unsupported cluster $cluster!</span><br />\n";
+          $filenames = array();
+          break;
+    }
+   
+    $save_cwd = getcwd();         // So we can come back to the current 
+                                  // working directory later
+
+    foreach ( $filenames as $filename )
+    {
+      chdir( dirname( $filename ) );
+
+      $job-> clear();
+      $job-> parse_input( basename( $filename ) );
+      if ( ! DEBUG ) $job-> submit();
+      $retval = $job->get_messages();
+
+      if ( ! empty( $retval ) )
+      {
+        $output_msg .= "<br /><span class='message'>Message from the queue...</span><br />\n" .
+                        print_r( $retval, true ) . " <br />\n";
+      }
+    }
+
+    chdir( $save_cwd );
+  }
+  $output_msg .= "</pre>\n";
+}
+
+else
+{
+  $output_msg = <<<HTML
+  Thank you, there have been one or more problems writing the various files necessary
+  for job submission. Please contact your system administrator.
+
+HTML;
+
+}
+
+// Start displaying page
+$page_title = 'PCSA Analysis Submitted';
+include 'header.php';
+
+$message = ( isset( $message ) ) ? "<p class='message'>$message</p>" : "";
+$show = $payload->show( $HPCAnalysisRequestID, $filenames );  // debugging info, if enabled
+
+echo <<<HTML
+<!-- Begin page content -->
+<div id='content'>
+
+  <h1 class="title">$page_title</h1>
+  <!-- Place page content here -->
+
+  $message
+  <p>$output_msg</p>
+
+  <p><a href="queue_setup_1.php">Submit another request</a></p>
+
+  $show
+
+</div>
+
+HTML;
+
+include 'footer.php';
+exit();
+
+?>
